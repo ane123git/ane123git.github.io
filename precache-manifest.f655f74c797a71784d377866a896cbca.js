90864157a0f088cd38607d91fb4a025bdbbc2af3
aneesh.bhagat <aneesh.bhagat@accenture.com>
2019-12-04 20:09:22 +0000

    Updates

---
+++ precache-manifest.f655f74c797a71784d377866a896cbca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a141bc91365bbc240d38f075efb391a",
+    "url": "/ane123git.github.io/index.html"
+  },
+  {
+    "revision": "307e0bd1602112b9eba1",
+    "url": "/ane123git.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5f53671c97aedc70791c",
+    "url": "/ane123git.github.io/static/js/2.611fe376.chunk.js"
+  },
+  {
+    "revision": "307e0bd1602112b9eba1",
+    "url": "/ane123git.github.io/static/js/main.041f21ec.chunk.js"
+  },
+  {
+    "revision": "b1fbd7460728c6cd8c7f",
+    "url": "/ane123git.github.io/static/js/runtime-main.135303ce.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ane123git.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ane123git.github.io/precache-manifest.f655f74c797a71784d377866a896cbca.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ane123git.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
