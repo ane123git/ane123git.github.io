5523219ee1a7476f171202ca62604f220f9d0db6
ane123git <58523914+ane123git@users.noreply.github.com>
2019-12-04 20:58:51 +0000

    aaaaa

--- src/index.js
+++ src/index.js
@@ -6,6 +6,24 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+  function traceCaller(n) {
+    if( isNaN(n) || n<0) n=1;
+    n+=1;
+    var s = (new Error()).stack
+      , a=s.indexOf('\n',5);
+    while(n--) {
+      a=s.indexOf('\n',a+1);
+      if( a<0 ) { a=s.lastIndexOf('\n',s.length); break;}
+    }
+    b=s.indexOf('\n',a+1); if( b<0 ) b=s.length;
+    a=Math.max(s.lastIndexOf(' ',b), s.lastIndexOf('/',b));
+    b=s.lastIndexOf(':',b);
+    s=s.substring(a+1,b);
+    return s;
+  }
+
+console.log("ssssssssssssss "+traceCaller(1))
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
